b15ce23d4fc7c209b44f896c13bde079cde2f673
Pedro Henrick <phmc@ic.ufal.br>
2022-05-25 16:11:25 -0300

    feat(product-controller): criando router.get
    Adicionando get a rota /products, utilizando try/catch para lidar com erros

---
+++ controllers/productController.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const productService = require('../services/productService');
+
+const router = express.Router();
+
+router.get('/', async (_req, res) => {
+  try {
+    const [rows] = productService.get();
+    res.status(200).json(rows);
+  } catch (err) {
+    res.status(500).json({ Error: 'Erro interno' });
+  }
+});
+
+module.exports = router;

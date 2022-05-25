5fab1562deacad2201c5df15bcb21402a6b9bc20
Pedro Henrick <phmc@ic.ufal.br>
2022-05-25 15:56:31 -0300

    feat(product-model): criando getAll
    Foi adicionado a esse arquivo a função getAll, que será usada para pegar todos os dados que estão na
    tabela products do bando de dados

---
+++ models/productModel.js
@@ -0,0 +1,8 @@
+const connection = require('../database/connection');
+
+const getAll = () => {
+  const query = 'SELECT * FROM products';
+  return connection.execute(query);
+};
+
+module.exports = getAll;

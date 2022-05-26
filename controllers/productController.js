527afbbc2fc4c1536459e61b52864a627693a58f
Pedro Henrick <phmc@ic.ufal.br>
2022-05-26 18:25:43 -0300

    perf(product-controller): removendo regra de negócio

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 router.get('/', async (_req, res) => {
   try {
-    const [rows] = await productService.get();
-    res.status(200).json(rows);
+    const products = await productService.get();
+    res.status(200).json(products);
   } catch (err) {
     res.status(500).json({ Error: 'Erro interno' });
   }
@@ -15,14 +15,10 @@ router.get('/', async (_req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const [rows] = await productService.get();
+    const product = await productService.get(id);
     
-    const row = rows.find((product) => product.id === +id); 
-    
-    if (!row) {
-      res.status(404).json({ message: 'Product not found' });
-    }
-    res.status(200).json(row);
+    if (!product) res.status(404).json({ message: 'Product not found' });
+    res.status(200).json(product);
   } catch (err) {
     res.status(500).json({ Error: 'Erro interno' });
   }

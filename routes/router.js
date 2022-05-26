436e1cf2e5f3016405cc3dfbfd45aa752e2fe1ef
Pedro Henrick <phmc@ic.ufal.br>
2022-05-26 14:30:22 -0300

    feat(router-main): criando enpoint /sales
    Criando pasta e arquivo router que irão cuidar do redirecionamento via endpoint

--- routes/router.js
+++ routes/router.js
@@ -3,5 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/products', require('../controllers/productController'));
+router.use('/sales', require('../controllers/salesController'));
 
 module.exports = router;

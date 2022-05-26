02761e9c3d48dc0825f0165398ef6d4ca4e4ca00
Pedro Henrick <phmc@ic.ufal.br>
2022-05-26 18:34:04 -0300

    feat(sales-model): adicionando getById e refinado busca do getAll

--- models/salesModel.js
+++ models/salesModel.js
@@ -1,10 +1,25 @@
 const connection = require('../database/connection');
 
 const getAll = () => {
-  const query = 'SELECT * FROM sales';
+  const query = `SELECT DISTINCT sp.sale_id, sp.product_id, sp.quantity, s.date
+  FROM sales_products as sp
+  INNER JOIN sales s
+  ON s.id = sp.sale_id
+  ORDER BY sp.sale_id ASC, sp.product_id ASC;`;
   return connection.execute(query);
+};
+
+const getById = (id) => {
+  const query = `SELECT s.date, sp.product_id as productId, sp.quantity
+  FROM sales_products as sp
+  INNER JOIN sales s
+  ON s.id = sp.sale_id
+  WHERE sp.sale_id = ?
+  ORDER BY sp.sale_id ASC, sp.product_id ASC;`;
+  return connection.execute(query, [id]);
 };
 
 module.exports = {
   getAll,
+  getById,
 };

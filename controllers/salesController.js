a9fba991ba58c86014afc181ab737c226dfa48df
Pedro Henrick <phmc@ic.ufal.br>
2022-05-26 18:35:33 -0300

    feat(sales-controller): criando rota get /sales/:id

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -5,8 +5,20 @@ const router = express.Router();
 
 router.get('/', async (_req, res) => {
   try {
-    const [rows] = await salesService.get();
-    res.status(200).json(rows);
+    const sales = await salesService.get();
+    res.status(200).json(sales);
+  } catch (err) {
+    res.status(500).json({ Error: 'Erro interno' });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const sale = await salesService.get(id);
+
+    if (!sale) res.status(404).json({ message: 'Sale not found' });
+    res.status(200).json(sale);
   } catch (err) {
     res.status(500).json({ Error: 'Erro interno' });
   }

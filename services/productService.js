f94a9cac2b0a834962d7edc752f01b6ec843fe0b
Pedro Henrick <phmc@ic.ufal.br>
2022-05-26 18:29:33 -0300

    feat(product-service): adicionando regra de negócio

--- services/productService.js
+++ services/productService.js
@@ -1,6 +1,14 @@
 const productModel = require('../models/productModel');
 
-const get = async () => productModel.getAll();
+const get = async (id = null) => {
+  const [rows] = await productModel.getAll();
+  
+  if (id) {
+    const row = rows.find((product) => product.id === +id);
+    return row;
+  }
+  return rows;
+};
 
 module.exports = {
   get,

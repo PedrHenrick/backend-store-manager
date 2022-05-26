f4b5b23c37c5ade076a833820361e57561a0cd1d
Pedro Henrick <phmc@ic.ufal.br>
2022-05-25 22:25:28 -0300

    feat(product-controller): criando endpoint /products/:id
    Adicionando rota que pega o usuário com o id pedido, já verificando se o elemento existe ou não e
    retornando o erro 'Product not found'

--- controllers/productController.js
+++ controllers/productController.js
@@ -12,4 +12,20 @@ router.get('/', async (_req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [rows] = await productService.get();
+    
+    const row = rows.find((product) => product.id === +id); 
+    
+    if (!row) {
+      res.status(404).json({ message: 'Product not found' });
+    }
+    res.status(200).json(row);
+  } catch (err) {
+    res.status(500).json({ Error: 'Erro interno' });
+  }
+});
+
 module.exports = router;

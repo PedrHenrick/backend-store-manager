9400fdebf3fd3ed32f191a87daf21c6eabb60db9
Pedro Henrick <phmc@ic.ufal.br>
2022-05-26 18:34:56 -0300

    feat(sales-service): adicionando verificação por id

--- services/salesService.js
+++ services/salesService.js
@@ -1,6 +1,17 @@
 const salesModel = require('../models/salesModel');
+const { serializeSale } = require('../utils');
 
-const get = async () => salesModel.getAll();
+const get = async (id = null) => {
+  if (id) {
+    const [rows] = await salesModel.getById(id);
+    if (rows.length === 0) return undefined;
+    return rows;
+  }
+  
+  const [rows] = await salesModel.getAll();
+  const sales = rows.map((row) => serializeSale(row));
+  return sales;
+};
 
 module.exports = {
   get,

f2ffa272b88f67ae2131bb4edcf392d4b2f2740b
Pedro Henrick <phmc@ic.ufal.br>
2022-05-27 15:49:41 -0300

    docs(update): requisito 1 e 2 passando

--- routes/router.js
+++ routes/router.js
@@ -3,9 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 // router.use('/products', require('../notController/productControllerOrig'));
-router.use('/products', require('./productRouter'));
-
 // router.use('/sales', require('../notController/salesControllerOrig'));
+
+router.use('/products', require('./productRouter'));
 router.use('/sales', require('./saleRouter'));
 
 module.exports = router;

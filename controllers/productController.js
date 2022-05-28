62f7dc38e851c7c47bc70e13ee635b41a21a65dd
Pedro Henrick <phmc@ic.ufal.br>
2022-05-28 17:08:32 -0300

    feat(product-controller): criação da função update

--- controllers/productController.js
+++ controllers/productController.js
@@ -36,8 +36,24 @@ const add = async (req, res) => {
   }
 };
 
+const update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const newProduct = req.body;
+
+    const updatedProduct = await productService.update(id, newProduct);
+    
+    if (!updatedProduct) return res.status(404).json({ message: 'Product not found' });
+    
+    return res.status(200).json(updatedProduct);
+  } catch (err) {
+    return res.status(500).json({ Error: 'Erro interno' });
+  }
+};
+
 module.exports = {
   getAll,
   getById,
   add,
+  update,
 };

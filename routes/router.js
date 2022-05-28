fc7b1cc633077c66e369217c2113bda8204709e4
Pedro Henrick <phmc@ic.ufal.br>
2022-05-28 17:36:35 -0300

    docs(update): requisito 6

--- routes/router.js
+++ routes/router.js
@@ -2,8 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-// router.use('/sales', require('../notController/salesControllerOrig'));
-
 router.use('/products', require('./productRouter'));
 router.use('/sales', require('./saleRouter'));
 

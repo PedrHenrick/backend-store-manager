ea82d4b0807141927cb0fd5cb670ebcfe671d628
Pedro Henrick <phmc@ic.ufal.br>
2022-05-27 23:59:28 -0300

    feat(product-router): adicionando rota post
    Foi adicionada a rota post para cuidar das inserções no bando de dados, além disso, usamos o método
    express.json pra identificar o tipo de arquivo que será retornado e aplicando os middlewares a rota

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,9 +1,12 @@
 const express = require('express/');
 const productController = require('../controllers/productController');
+const { validateProduct, validateLengthProduct } = require('../middlewares/validateProduct');
 
 const productRouter = express.Router();
+productRouter.use(express.json());
 
 productRouter.get('/', productController.getAll);
 productRouter.get('/:id', productController.getById);
+productRouter.post('/', validateProduct, validateLengthProduct, productController.add);
 
 module.exports = productRouter;

562c0e43e58d315a466971fbeadb927f69149821
Pedro Henrick <phmc@ic.ufal.br>
2022-05-28 00:00:47 -0300

    docs(update): requisito 4 passando e requisito 3 iniciado

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -3,6 +3,7 @@ const productController = require('../controllers/productController');
 const { validateProduct, validateLengthProduct } = require('../middlewares/validateProduct');
 
 const productRouter = express.Router();
+
 productRouter.use(express.json());
 
 productRouter.get('/', productController.getAll);

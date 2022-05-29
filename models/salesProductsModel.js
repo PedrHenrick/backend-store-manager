1f4c09a26c50ee2b6111b48c6d7bbc01207a624a
Pedro Henrick <phmc@ic.ufal.br>
2022-05-28 21:59:02 -0300

    feat(salesproducts-model): criando função update
    Função responsável por editar os vendas na tabela sales_products

--- models/salesProductsModel.js
+++ models/salesProductsModel.js
@@ -10,6 +10,17 @@ const add = async (id, { productId, quantity }) => {
   };
 };
 
+const update = async (id, { productId, quantity }) => {
+  const query = 'UPDATE sales_products SET quantity = ? WHERE sale_id = ? AND product_id = ?;';
+  await connection.execute(query, [quantity, id, productId]);
+  
+  return {
+    productId,
+    quantity,
+  };
+};
+
 module.exports = {
   add,
+  update,
 };

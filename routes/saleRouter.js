00e4f22ab1db05d2fac85b3f0818805009686cd0
Pedro Henrick <phmc@ic.ufal.br>
2022-05-28 21:10:22 -0300

    feat(sale-route): criando rota post e adicionando middleware de validação

--- routes/saleRouter.js
+++ routes/saleRouter.js
@@ -1,9 +1,13 @@
 const express = require('express/');
 const salesController = require('../controllers/salesController');
+const { validateSale, validateLengthSale } = require('../middlewares/validateSale');
 
 const saleRouter = express.Router();
 
+saleRouter.use(express.json());
+
 saleRouter.get('/', salesController.getAll);
 saleRouter.get('/:id', salesController.getById);
+saleRouter.post('/', validateSale, validateLengthSale, salesController.add);
 
 module.exports = saleRouter;

646f4f60545509d1bf8d385c4932ce75d90275a7
Pedro Henrick <phmc@ic.ufal.br>
2022-05-28 21:09:20 -0300

    feat(sales-controller): criando função add e executando regras de negócio

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -3,9 +3,10 @@ const salesService = require('../services/salesService');
 const getAll = async (_req, res) => {
   try {
     const sales = await salesService.get();
-    res.status(200).json(sales);
+    
+    return res.status(200).json(sales);
   } catch (err) {
-    res.status(500).json({ Error: 'Erro interno' });
+    return res.status(500).json({ Error: 'Erro interno' });
   }
 };
 
@@ -14,14 +15,31 @@ const getById = async (req, res) => {
     const { id } = req.params;
     const sale = await salesService.get(id);
 
-    if (!sale) res.status(404).json({ message: 'Sale not found' });
-    res.status(200).json(sale);
+    if (!sale) return res.status(404).json({ message: 'Sale not found' });
+    
+    return res.status(200).json(sale);
   } catch (err) {
     res.status(500).json({ Error: 'Erro interno' });
+  }
+};
+
+const add = async (req, res) => {
+  try {
+    const newSale = req.body;
+
+    const sale = await salesService.add(newSale);
+    
+    if (!sale) return res.status(502).json({ Error: 'Foi recebido valor inválido do servidor' });
+
+    return res.status(201).json(sale);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ Error: 'Erro interno' });
   }
 };
 
 module.exports = {
   getAll,
   getById,
+  add,
 };

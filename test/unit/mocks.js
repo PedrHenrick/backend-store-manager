c6937b89e8280a5db01a4318df47e94b344e67d3
Pedro Henrick <phmc@ic.ufal.br>
2022-05-31 22:04:15 -0300

    feat(mocks): adicionando mocks extras

--- test/unit/mocks.js
+++ test/unit/mocks.js
@@ -16,9 +16,25 @@ const productsAll = [
   }
 ]
 
-const productId = {
+const productId = [{
   "id": 1,
   "name": "Martelo de Thor",
+  "quantity": 10
+}]
+
+const newProduct = {
+  "name": "Bananinha",
+  "quantity": 10
+}
+
+const existingProduct = {
+  "name": "Martelo de Thor",
+  "quantity": 10
+}
+
+const product = {
+  "id": 99,
+  "name": "Bananinha",
   "quantity": 10
 }
 
@@ -43,6 +59,26 @@ const salesAll = [
   }
 ]
 
+const allSales = [
+  { "id": 1, "date": "2022-05-31T03:55:07.000Z" },
+  { "id": 2, "date": "2022-05-31T03:55:07.000Z" }
+]
+
+const newSale = {
+  "productId": 1,
+  "quantity": 3
+}
+
+const responseSale = {
+  "id": 1,
+  "itemsSold": [
+    {
+      "productId": 1,
+      "quantity": 3
+    }
+  ]
+}
+
 const saleId = [
   {
       "date": "2022-05-27T16:01:51.000Z",
@@ -56,9 +92,41 @@ const saleId = [
   }
 ]
 
+const saleUpdated = {
+  "saleId": 1,
+  "itemUpdated": [
+    {
+      "productId": 1,
+      "quantity": 6
+    }
+  ]
+}
+
+const productObjectAdd = { "name": "produto", "quantity": 10 }
+
+const productResponseAddTrue = [{ "id": 1, "name": "produto", "quantity": 10 }]
+
+const saleObjectAdd = { "name": "produto", "quantity": 10 }
+
+const saleProductsResponseAddTrue =  [{ "id": 1, "name": "produto", "quantity": 10 }]
+
+const saleResponseAddTrue = [{ "id": 1, "date": "2022-05-27T16:01:51.000Z" }]
+
 module.exports = {
   productsAll,
   productId,
   salesAll,
   saleId,
+  productObjectAdd,
+  productResponseAddTrue,
+  saleObjectAdd,
+  saleResponseAddTrue,
+  saleProductsResponseAddTrue,
+  newProduct,
+  product,
+  existingProduct,
+  newSale,
+  responseSale,
+  saleUpdated,
+  allSales,
 }

9258272511825f1be9093c44d7c0e11cec1eebee
Pedro Henrick <phmc@ic.ufal.br>
2022-05-31 22:02:28 -0300

    feat(app): adicionando middleware de erro

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/', (_request, response) => {
 
 app.use(require('./routes/router'));
 
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500).json({ message: err.message || 'Erro interno' });
+});
+
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 

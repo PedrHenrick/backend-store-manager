deef9d7173da9760b9c771d20702369052b09667
Pedro Henrick <phmc@ic.ufal.br>
2022-06-05 19:22:46 -0300

    refactor(sale-controller): removendo linhas e retirando condicionais

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,53 +1,29 @@
 const salesService = require('../services/salesService');
 
 const getAll = async (_req, res) => {
-  const sales = await salesService.get();
-  
+  const [sales] = await salesService.getAll();
+  console.log('sales=> ', sales);
   return res.status(200).json(sales);
 };
 
 const getById = async (req, res) => {
-  const { id } = req.params;
-  const sale = await salesService.get(id);
-
-  if (!sale) return res.status(404).json({ message: 'Sale not found' });
-  
+  const sale = await salesService.getById(req.params);  
   return res.status(200).json(sale);
 };
 
 const add = async (req, res) => {
-  const newSale = req.body;
-
-  const sale = await salesService.add(newSale);
-    
+  const sale = await salesService.add(req.body);
   return res.status(201).json(sale);
 };
 
 const update = async (req, res) => {
-  const { id } = req.params;
-  const editSale = req.body;
-
-  const sale = await salesService.update(id, editSale);
-
-  if (!sale) return res.status(404).json({ message: 'Sale not found' });
-
+  const sale = await salesService.update(req.params, req.body);
   return res.status(200).json(sale);
 };
 
 const deleteProduct = async (req, res) => {
-  const { id } = req.params;
-
-  const result = await salesService.deleteProduct(id);
-
-  if (!result) return res.status(404).json({ message: 'Sale not found' });
-
+  await salesService.deleteProduct(req.params);
   return res.status(204).end();
 };
 
-module.exports = {
-  getAll,
-  getById,
-  add,
-  update,
-  deleteProduct,
-};
+module.exports = { getAll, getById, add, update, deleteProduct };

e1e96f7131c8d2d09e6603c211dda5967789ce9c
Pedro Henrick <phmc@ic.ufal.br>
2022-06-05 19:25:23 -0300

    refactor(sale-service): removendo linhas, criando getById e refatorando lógica

--- services/salesService.js
+++ services/salesService.js
@@ -1,69 +1,47 @@
 const salesModel = require('../models/salesModel');
+
+const objectErrorNotFound = { status: 404, message: 'Sale not found' };
 const salesProductsModel = require('../models/salesProductsModel');
 const actualizeItems = require('../utils');
 
-const get = async (id = null) => {
-  if (id) {
-    const [rows] = await salesModel.getById(id);
-    if (rows.length === 0) return undefined;
-    return rows;
-  }
-  
-  const [rows] = await salesModel.getAll();
-  const sales = rows.map((row) => actualizeItems.serializeSale(row));
-  return sales;
+const getAll = async () => salesModel.getAll();
+
+const getById = async ({ id }) => {
+  const [rows] = await salesModel.getById(id);
+  if (!rows[0]) throw objectErrorNotFound;
+  return rows;
 };
 
 const add = async (sales) => {
   await actualizeItems.addItem(sales);
-
   const { id } = await salesModel.add();
 
   const registeredSales = await Promise.all(sales
-  .map((sale) => salesProductsModel.add(id, sale)));
-  
-  return {
-    id,
-    itemsSold: registeredSales,
-  };
+    .map((sale) => salesProductsModel.add(id, sale)));
+
+  return { id, itemsSold: registeredSales };
 };
 
-const update = async (id, sales) => {
-  const [rows] = await salesModel.getAllSale();
+const update = async ({ id }, sales) => {
+  const [rows] = await salesModel.getAllSaleById(id);
   
-  const verify = rows.find((row) => row.id === +id);
-  
-  if (!verify) return undefined;
-
+  if (!rows[0]) throw objectErrorNotFound;
   await actualizeItems.updateItem(id, sales);
   
   const editedSales = await Promise.all(sales
     .map((sale) => salesProductsModel.update(id, sale)));
-  
-  return {
-    saleId: id,
-    itemUpdated: editedSales,
-  };
+
+  return { saleId: id, itemUpdated: editedSales };
 };
 
-const deleteProduct = async (id) => {
-  const [rows] = await salesModel.getAllSale();
-  
-  const verify = rows.find((row) => row.id === +id);
-  
-  if (!verify) return undefined;
-  
+const deleteProduct = async ({ id }) => {
+  const [rows] = await salesModel.getAllSaleById(id);  
+  if (!rows[0]) throw objectErrorNotFound;
+
   await actualizeItems.deleteItem(id);
-  
   await salesModel.deleteProduct(id);
   await salesProductsModel.deleteProduct(id);
-
   return true;
 };
 
-module.exports = {
-  get,
-  add,
-  update,
-  deleteProduct,
-};
+module.exports = { getAll, getById, add, update, deleteProduct };

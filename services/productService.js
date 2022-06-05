f009b0345750731f6ab31c7ab93b4049facb119c
Pedro Henrick <phmc@ic.ufal.br>
2022-06-05 16:33:49 -0300

    refactor(product-service): disparando exceções de erro quando o retorno for undefined
    Adicionando objeto e exceções de erros para serem capturados pelo middleware de erro, removendo o
    uso de condicionais no product controller

--- services/productService.js
+++ services/productService.js
@@ -1,10 +1,14 @@
 const productModel = require('../models/productModel');
+const objectErrorNotFound = { status: 404, message: 'Product not found' };
+const objectErrorAlreadyExists = { status: 409, message: 'Product already exists' };
 
 const get = async (id = null) => {
   const [rows] = await productModel.getAll();
-  
+
   if (id) {
     const row = rows.find((product) => product.id === +id);
+    
+    if (!row) throw objectErrorNotFound;
     return row;
   }
   return rows;
@@ -12,34 +16,25 @@ const get = async (id = null) => {
 
 const add = async (newProduct) => {
   const [rows] = await productModel.getAll();
-
   const row = rows.find((product) => product.name === newProduct.name);
 
-  if (!row) {
-    return productModel.add(newProduct);
-  }
-  return undefined;
+  if (!row) return productModel.add(newProduct);
+  throw objectErrorAlreadyExists;
 };
 
 const update = async (id, newProduct) => {
   const [rows] = await productModel.getAll();
-
   const row = rows.find((product) => product.id === +id);
 
-  if (!row) {
-    return undefined;
-  }
+  if (!row) throw objectErrorNotFound;
   return productModel.update(id, newProduct);
 };
 
 const deleteProduct = async (id) => {
   const [rows] = await productModel.getAll();
-
   const row = rows.find((product) => product.id === +id);
 
-  if (!row) {
-    return undefined;
-  }
+  if (!row) throw objectErrorNotFound;
   return productModel.deleteProduct(id);
 };
 

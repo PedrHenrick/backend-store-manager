f24a6950e4ec47143a9f0cc3e59adcd9a0619f98
Pedro Henrick <phmc@ic.ufal.br>
2022-06-05 19:18:19 -0300

    style(product-model): removendo linhas

--- models/productModel.js
+++ models/productModel.js
@@ -7,20 +7,17 @@ const getById = (id) => connection.execute('SELECT * FROM products WHERE id = ?'
 const add = async ({ name, quantity }) => {
   const [row] = await connection.execute(`INSERT INTO products (name, quantity)
   VALUES (?, ?)`, [name, quantity]);
-
   return { id: row.insertId, name, quantity };
 };
 
 const update = async (id, { name, quantity }) => {
   await connection.execute(`UPDATE products 
   SET name = ?, quantity = ? WHERE id = ?`, [name, quantity, id]);
-
   return { id, name, quantity };
 };
 
 const deleteProduct = async (id) => {
   await connection.execute('DELETE FROM products WHERE id = ?', [id]);
-  
   return { id };
 };
 

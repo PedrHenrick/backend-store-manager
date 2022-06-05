903985b0691eafcf0fa7853ff7fc993da52090b8
Pedro Henrick <phmc@ic.ufal.br>
2022-06-05 19:28:06 -0300

    refactor(sale-model): criando getAllSaleById, removendo linhas e refatorando a lógica

--- models/salesModel.js
+++ models/salesModel.js
@@ -1,47 +1,32 @@
 const connection = require('../database/connection');
 
-const getAll = () => {
-  const query = `SELECT DISTINCT sp.sale_id, sp.product_id, sp.quantity, s.date
-  FROM sales_products as sp
-  INNER JOIN sales s
-  ON s.id = sp.sale_id
-  ORDER BY sp.sale_id ASC, sp.product_id ASC;`;
-  return connection.execute(query);
-};
+const getAll = () => connection.execute(`
+SELECT DISTINCT sp.sale_id as saleId, 
+  sp.product_id as productId, 
+  sp.quantity, s.date
+FROM sales_products as sp
+INNER JOIN sales s
+ON s.id = sp.sale_id
+ORDER BY sp.sale_id ASC, sp.product_id ASC;`);
 
-const getAllSale = () => {
-  const query = 'SELECT * FROM sales;';
-  return connection.execute(query);
-};
+const getAllSale = () => connection.execute('SELECT * FROM sales;');
 
-const getById = (id) => {
-  const query = `SELECT s.date, sp.product_id as productId, sp.quantity
-  FROM sales_products as sp
-  INNER JOIN sales s
-  ON s.id = sp.sale_id
-  WHERE sp.sale_id = ?
-  ORDER BY sp.sale_id ASC, sp.product_id ASC;`;
-  return connection.execute(query, [id]);
-};
+const getAllSaleById = (id) => connection.execute('SELECT * FROM sales WHERE id = ?;', [id]);
+
+const getById = (id) => connection.execute(`SELECT s.date, 
+  sp.product_id as productId,
+  sp.quantity
+FROM sales_products as sp
+INNER JOIN sales s
+ON s.id = sp.sale_id
+WHERE sp.sale_id = ?
+ORDER BY sp.sale_id ASC, sp.product_id ASC;`, [id]);
 
 const add = async () => {
-  const query = 'INSERT INTO sales (date) VALUES (NOW());';
-  const [rows] = await connection.execute(query);
-
-  return {
-    id: rows.insertId,
-  };
+  const [rows] = await connection.execute('INSERT INTO sales (date) VALUES (NOW());');
+  return { id: rows.insertId };
 };
 
-const deleteProduct = async (id) => {
-  const query = 'DELETE FROM sales WHERE id = ?;';
-  return connection.execute(query, [id]);
-};
+const deleteProduct = async (id) => connection.execute('DELETE FROM sales WHERE id = ?;', [id]);
 
-module.exports = {
-  getAll,
-  getAllSale,
-  getById,
-  add,
-  deleteProduct,
-};
+module.exports = { getAll, getAllSale, getAllSaleById, getById, add, deleteProduct };
